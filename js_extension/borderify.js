475d1567c4c511a317780a54a23846519c35796c
bob-icsg <bobzhou@bu.edu>
2017-12-03 14:20:34 -0500

    AUTOCOMMIT bobzhou@bobzhou-ThinkCentre-M73:Sat Dec  2 22:55:33 EST 2017

--- js_extension/borderify.js
+++ js_extension/borderify.js
@@ -3,7 +3,26 @@ console.log("Oh, jeez, I debugged");
 document.body.style.border = "5px solid red";
 
 // current url
-console.log(window.content.location.href);
+// console.log(window.content.location.href);
+function getDomain(url, subdomain) {
+    subdomain = subdomain || false;
+
+    url = url.replace(/(https?:\/\/)?(www.)?/i, '');
+
+    if (!subdomain) {
+        url = url.split('.');
+
+        url = url.slice(url.length - 2).join('.');
+    }
+
+    if (url.indexOf('/') !== -1) {
+        return url.split('/')[0];
+    }
+
+    return url;
+}
+
+console.log(getDomain(window.content.location.href));
 
 // string match
 var str = "The best things in life are free";
@@ -16,23 +35,60 @@ var my_img = "http://www.bu.edu/homepage/feature-images/sub/hoopheal.jpg"
 
 
 $(document).ready(function(){
+
+	var total_list = ""
 	
 	function reqListener () {
-  		console.log(this.responseText);
+		
+		current_domain = getDomain(window.content.location.href)
+		var patt = new RegExp(current_domain);
+		console.log("Patt: " + patt);
+
+		var url_list = this.responseText;
+		console.log("url_list: " + url_list);
+
+		var res = url_list.match(patt);
+		console.log("res: " + res);
+
+		if (res) {
+			console.log("We have the url");
+		} else {
+
+			console.log("We do not have the url");
+
+			function postListener () {
+				console.log("POST: " + this.responseText);
+			}
+
+			var postReq = new XMLHttpRequest();
+			postReq.addEventListener("load", postListener);
+			//oReq.open("GET", "https://www.mozilla.org");
+			postReq.open("POST", "http://128.197.127.31:1234");
+			postReq.send("eval&" + current_domain);
+
+
+		}
 	}
 
 	var oReq = new XMLHttpRequest();
 	oReq.addEventListener("load", reqListener);
 	//oReq.open("GET", "https://www.mozilla.org");
-	oReq.open("GET", "https://www.google.com");
+	oReq.open("GET", "http://128.197.127.31:1234/home/ec521/ec521_final_project/blacklist.txt");
 	oReq.send();
+
 	console.log('end');
-	
-	$.get('test.txt', function(data) {
-		console.log(data)
-	}, 'text');
+
 	
 });
+
+var reader = new FileReader();
+
+reader.onload = function(e) {
+	var text = reader.result;
+	console.log(text);
+}
+
+reader.readAsData('test.txt');
 
 console.log("Oh, jeez, I debugged in the end");
 
