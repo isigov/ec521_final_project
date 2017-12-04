634f1b4f9e8daef4e7f73897d26b516494277a78
zungur <zungur@bu.edu>
2017-12-04 09:48:13 -0500

    mini-crawler done

---
+++ clean_profile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ec521/ec521_final_project/cleanProfile
+rm -rf !(times.json)
--- mini-crawler.py
+++ mini-crawler.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python
 
 from selenium import webdriver
-import time, sys, pickle
+import time, sys, pickle, os
 
 
 def crawl_url(url):
 
-	# profile = webdriver.FirefoxProfile()
 	# profile.set_preference("browser.cache.disk.enable", False)
 	# profile.set_preference("browser.cache.memory.enable", False)
 	# profile.set_preference("browser.cache.offline.enable", False)
@@ -16,9 +15,7 @@ def crawl_url(url):
 	driver = webdriver.Firefox()
 	# user_agent = driver.execute_script("return navigator.userAgent")
 
-	driver.get(url)
-	create_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[1]")
-	
+
 
 	def save_cookies(dumpName):
 		cookies_list = driver.get_cookies()
@@ -29,7 +26,11 @@ def crawl_url(url):
 
 		pickle.dump( cookies_list, open(dumpName,"wb"))
 
-	create_button[0].click()
+
+	driver.get(url)
+	if (url == "http://mary008-340stat01.bu.edu" or url =="http://samy.pl/evercookie"):
+		create_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[1]")
+		create_button[0].click()
 	time.sleep(5)
 	save_cookies("crawl1.pkl")
 
@@ -37,59 +38,105 @@ def crawl_url(url):
 	driver.delete_all_cookies()
 
 	driver.get(url)
-	discover_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[2]")
-	discover_button[0].click()
+	if (url == "http://mary008-340stat01.bu.edu" or url =="http://samy.pl/evercookie"):
+		discover_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[2]")
+		discover_button[0].click()
 	time.sleep(5)
 	save_cookies("crawl2.pkl")
-
-
+	
 	driver.quit()
 
-	driver = webdriver.Firefox("/home/ec521/Desktop/cleanProfile")
+	os.system("/home/ec521/ec521_final_project/clean_profile.sh")
+	driver = webdriver.Firefox("/home/ec521/ec521_final_project/cleanProfile")
+
 	driver.get(url)
-	discover_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[2]")
-	discover_button[0].click()
+	if (url == "http://mary008-340stat01.bu.edu" or url =="http://samy.pl/evercookie"):
+		discover_button = driver.find_elements_by_xpath("//*[@id=\"pagecontent\"]/pre[3]/input[2]")
+		discover_button[0].click()
 	time.sleep(5)
 	save_cookies("crawl3.pkl")
 	driver.quit()
 
-
+def findCookieValue(cookie, name):
+	for i in range(len(cookie)):
+		if cookie[i]['name'] == name:
+			return cookie[i]['value']
+	return None
 
 def memory_load(fileName):
 	with open(fileName, 'rb') as handle:
 		library = pickle.load(handle)
 		return library
 
+def everCookieFind(cookieDB1, cookieDB2, cookieDB3):
+	evercookiePresent = False
+	cookie_name=[0 for i in range(len(cookieDB1))]
 
-url = "http://mary008-340stat01.bu.edu"
-url2 = "http://samy.pl/evercookie"
-file1 = "crawl1.pkl"
-file2 = "crawl2.pkl"
-file3 = "crawl3.pkl"
-
-#crawl_url(url2)
-
-
-lib1 = memory_load(file1)
-lib2 = memory_load(file2)
-lib3 = memory_load(file3)
-
-print("\n")
-
-for cookie_index in range(0, len(lib1)):
-	print(lib1[cookie_index]['name'], lib1[cookie_index]['value'])
-
-print("\n")
-
-for cookie_index in range(0, len(lib2)):
-	print(lib2[cookie_index]['name'], lib2[cookie_index]['value'])
-
-print("\n")
-
-for cookie_index in range(0, len(lib3)):
-	print(lib3[cookie_index]['name'], lib3[cookie_index]['value'])
-
+	for cookie_index in range(0, len(cookieDB1)):
+		cookie_name[cookie_index] = cookieDB1[cookie_index]['name']
 
 # if cookie same in 1,2,3 then disregard
 # if cookie same in 1 and 2 the evercookie
 # if cookie different in all then normal cookie
+	print "===================================="
+	for cname in cookie_name:
+		cookie1 = findCookieValue(cookieDB1, cname)
+		cookie2 = findCookieValue(cookieDB2, cname)
+		cookie3 = findCookieValue(cookieDB3, cname)
+		if cookie1 != None and cookie2 != None and cookie3 != None:
+			# disregard
+			if cookie1 == cookie2 == cookie3:
+				pass
+			# evercookie
+			if cookie1 == cookie2 != cookie3:
+				print "Potential EverCookie!!!!   " + cname
+				evercookiePresent = True
+			# normal cookie
+			if cookie1 != cookie2 != cookie3:
+				print "Normal Cookie              " + cname
+
+	return evercookiePresent
+
+
+def printCookie(cookieDB):
+	for cookie_index in range(0, len(cookieDB)):
+		print(cookieDB[cookie_index]['name'], cookieDB[cookie_index]['value'])
+	print("\n")
+
+def detect_evercookie(url):
+
+	file1 = "crawl1.pkl"
+	file2 = "crawl2.pkl"
+	file3 = "crawl3.pkl"
+
+	crawl_url(url)
+
+	lib1 = memory_load(file1)
+	lib2 = memory_load(file2)
+	lib3 = memory_load(file3)
+
+
+	#for cookie_index in range(0, len(lib2)):
+	#	print(lib2[cookie_index]['name'], lib2[cookie_index]['value'])
+
+	#for cookie_index in range(0, len(lib3)):
+	#	print(lib3[cookie_index]['name'], lib3[cookie_index]['value'])
+
+	printCookie(lib1)
+	printCookie(lib2)
+	printCookie(lib3)
+			
+	return everCookieFind(lib1,lib2,lib3)
+
+
+if __name__ == "__main__":
+
+	url = "http://yandex.ru"
+	url2="http://mary008-340stat01.bu.edu"
+	url3="http://samy.pl/evercookie"
+	
+	if(detect_evercookie(url)):
+		print("evercookie found!")
+	else:
+		print("All good")
+
