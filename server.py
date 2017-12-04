6a0e3d9347b2ea31823bc58886820d377368359d
Emre Ates <ates@bu.edu>
2017-12-04 09:46:22 -0500

    Eval stuff in server

--- server.py
+++ server.py
@@ -10,15 +10,15 @@ TODO:
 -starting crawler
 -getting crawl results back
 -updating blacklist
--sending (compressed?) blacklist to soecified address/port 
+-sending (compressed?) blacklist to soecified address/port
 -handling 2+ consecutive requests from extension, csv is reset otherwise
--race conditions concerning csv file and blacklist 
+-race conditions concerning csv file and blacklist
 
 Usage:
     ./server.py [<port>]
 Send a POST request::
     curl -d "request_type&url1&url2..." http://localhost
-Available request types: 
+Available request types:
     eval: evaluate given URLs
     update: send updated URL list
 """
@@ -26,27 +26,37 @@ Available request types:
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer, csv
 
+import multiprocessing
+import queue
+from sys import argv
 
-#TODO we are never receiving multiple URL's, better handle it here
-#TODO blacklist / whitelist / eval_queue
-#TODO add to eval_queue if not in all above
+from mini-crawler import detect_evercookie
+
+BLACKLIST = './blacklist.txt'
+WHITELIST = './whitelist.txt'
+
+def url_checker(inq):
+    while True:
+        website = inq.get()
+        if detect_evercookie(website):
+            with open(BLACKLIST, 'a') as f:
+                f.write(website)
+        else:
+            with open(WHITELIST, 'a') as f:
+                f.write(website)
 
 def url_eval(url_list):
-	num_urls = len(url_list)
-	index_list = list(range(num_urls+1))[1:]
-	for req in (url_list):
-		print(req)
-	
-	# we should be keeping a local black/whitelist file copy here 
-	#load them into url_list so that we don't evaluate twice
-	with open('/home/ec521/modCrawler/etc/eval_list.csv', 'wb') as eval_csv:
-		wr = csv.writer(eval_csv, quoting=csv.QUOTE_ALL)
-		wr.writerows(zip(index_list, url_list))	
+    num_urls = len(url_list)
+    index_list = list(range(num_urls+1))[1:]
+    for req in (url_list):
+        print(req)
+        eval_queue.put(req)
 
-# TODO
-def update_lists():
-	return True
-
+#    # we should be keeping a local black/whitelist file copy here
+#    #load them into url_list so that we don't evaluate twice
+#    with open('/home/ec521/modCrawler/etc/eval_list.csv', 'wb') as eval_csv:
+#        wr = csv.writer(eval_csv, quoting=csv.QUOTE_ALL)
+#        wr.writerows(zip(index_list, url_list))
 
 class S(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -77,23 +87,23 @@ class S(BaseHTTPRequestHandler):
 			self.wfile.write(f.read())
 			f.close()
 			return
-	
+
 	except Exception as e:
 		print(e)
 		self.send_error(404, 'file not found')
 
     def do_HEAD(self):
         self._set_headers()
-        
+
     def do_POST(self):
 	content_len = int(self.headers.getheader('content-length'))
 	post_body = self.rfile.read(content_len)
 	#print(post_body)
-	
+
 	post_body_list = post_body.split("&")
 	num_urls = len(post_body_list)-1
 	req_type = post_body_list[0]
-       
+
 	if (req_type == "eval"):
 		print("eval requested")
 		url_eval(post_body_list[1:])
@@ -106,16 +116,18 @@ class S(BaseHTTPRequestHandler):
 
 	self._set_headers()
         self.wfile.write("<html><body><h1>Request received!</h1></body></html>")
-        
+
 def run(server_class=HTTPServer, handler_class=S, port=80):
+    eval_queue = multiprocessing.Queue()
+    global eval_queue
+    eval_p = multiprocessing.Process(target=url_checker, args=(eval_queue,))
+    eval_p.start()
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     print 'Starting httpd...'
     httpd.serve_forever()
 
 if __name__ == "__main__":
-    from sys import argv
-
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
